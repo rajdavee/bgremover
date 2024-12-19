ed39c3ba08b79e7fa4c68933a469e7c1a58c03b2
rajdavee <rajdave710@gmail.com>
2024-12-19 17:53:29 +0530

    grgrgrg

--- main.py
+++ main.py
@@ -36,5 +36,5 @@ async def remove_background(file: UploadFile = File(...)):
 
 if __name__ == "__main__":
     # Bind to the PORT environment variable or default to 8000
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT",8080))
     uvicorn.run(app, host="0.0.0.0", port=port)
